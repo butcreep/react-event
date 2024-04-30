db69349dc5c8a44ceb888d7c567246664dae8b62
taga-dysong <dysong@tagababy.com>
2024-04-30 16:50:00 +0900

    회원가입 양식 수정

--- src/pages/Join/index.jsx
+++ src/pages/Join/index.jsx
@@ -1,7 +1,6 @@
-
 import React, { useEffect, useState } from "react";
-import { Input, Button, Radio, Form, DatePicker, Upload } from "antd";
-import { InboxOutlined } from "@ant-design/icons";
+import { Input, Button, Radio, Form } from "antd";
+
 import moment from "moment";
 const SubmitButton = ({ form, children }) => {
   const [submittable, setSubmittable] = useState(false);
@@ -16,6 +15,7 @@ const SubmitButton = ({ form, children }) => {
       .then(() => setSubmittable(true))
       .catch(() => setSubmittable(false));
   }, [form, values]);
+
   return (
     <Button type="primary" htmlType="submit" disabled={!submittable}>
       {children}
@@ -24,13 +24,8 @@ const SubmitButton = ({ form, children }) => {
 };
 const Join = () => {
   const [form] = Form.useForm();
-  const normFile = e => {
-    console.log("Upload event:", e);
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e?.fileList;
-  };
+  const [showAuthenticationCodeField, setShowAuthenticationCodeField] = useState(false);
+
   const validPhoneNumber = (phoneNumberInput, value) => {
     if (!value || !value.startsWith("010")) {
       return Promise.reject(new Error(phoneNumberInput.message));
@@ -44,48 +39,72 @@ const Join = () => {
 
     // 입력된 문자열에서 숫자만 추출하여 구성
     const phoneNumber = value.replace(/\D/g, "");
-
     // 전화번호 형식에 맞게 '-' 추가
     const formattedPhoneNumber = phoneNumber.replace(/(\d{3})(\d{3,4})(\d{4})/, "$1-$2-$3");
-
     // 폼 필드에 값을 설정
     form.setFieldsValue({ tel: formattedPhoneNumber });
   };
 
   const onFinish = values => {
-    console.log("Received values of form: ", values);
+    console.log("결과값: ", values);
   };
-  const disabledDate = current => {
-    // 현재 날짜에서 14년 전 날짜를 구함
-    const fourteenYearsAgo = moment().subtract(14, "years");
 
-    // 현재 날짜보다 이전인 경우만 비활성화
-    return current && current > fourteenYearsAgo.endOf("day");
-  };
   const handleInputChange = e => {
     const { value } = e.target;
     // 숫자 이외의 문자 제거
     const formattedValue = value.replace(/\D/g, "");
-
     // 구분자를 추가하여 출력
     const formattedDate = formattedValue
       .split("")
       .reduce((acc, char, index) => (index === 4 || index === 6 ? acc + "." + char : acc + char), "");
-
     // 폼 필드에 값을 설정
     form.setFieldsValue({ birthday: formattedDate });
   };
+  const handleFormChange = (changedValues, allValues) => {
+    // 모든 필드가 채워졌을 때 인증번호 입력 필드를 표시합니다.
+    const isFormFilled = Object.keys(allValues).every(key => allValues[key]);
+    setShowAuthenticationCodeField(isFormFilled);
+  };
+
   return (
     <div className="w-[400px] my-auto mx-auto">
-      <Form form={form} onFinish={onFinish} autoComplete="off">
+      <Form form={form} onFinish={onFinish} autoComplete="off" onValuesChange={handleFormChange}>
         <div className="flex gap-2 flex-col">
           <Form.Item
-            name="id"
             rules={[{ whitespace: true, required: true, message: "아이디는 필수로 입력해야 합니다!" }]}
+            validateTrigger="onBlur"
           >
             <Input placeholder="아이디 입력" />
           </Form.Item>
-          <Form.Item name="password" rules={[{ whitespace: true, required: true }]}>
+          <Form.Item
+            name="password"
+            rules={[
+              { whitespace: true },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value) {
+                    return Promise.reject(new Error("비밀번호를 입력해주세요"));
+                  }
+                  const hasUpperCase = /[A-Z]/.test(value);
+                  const hasLowerCase = /[a-z]/.test(value);
+                  const hasNumber = /[0-9]/.test(value);
+                  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+                  const isValid =
+                    (hasUpperCase ? 1 : 0) + (hasLowerCase ? 1 : 0) + (hasNumber ? 1 : 0) + (hasSpecialChar ? 1 : 0) >=
+                    2;
+                  if (value.length < 8 || value.length > 16 || !isValid) {
+                    return Promise.reject(
+                      new Error(
+                        "비밀번호 취약 : 8~16자의 영문 대/소문자, 숫자, 특수문자 중 2가지 이상을 사용해야 합니다.",
+                      ),
+                    );
+                  }
+                  return Promise.resolve();
+                },
+              }),
+            ]}
+            validateTrigger="onBlur"
+          >
             <Input type="password" placeholder="비밀번호 입력" />
           </Form.Item>
           <Form.Item
@@ -105,6 +124,7 @@ const Join = () => {
                 },
               }),
             ]}
+            validateTrigger="onBlur"
           >
             <Input type="password" placeholder="비밀번호 재확인" />
           </Form.Item>
@@ -114,6 +134,7 @@ const Join = () => {
             rules={[
               { whitespace: true, required: true, type: "email", message: "아이디를 이메일 형태로 입력해주세요" },
             ]}
+            validateTrigger="onBlur"
           >
             <Input placeholder="이메일 입력" />
           </Form.Item>
@@ -133,16 +154,6 @@ const Join = () => {
             <Input placeholder="이름" />
           </Form.Item>
 
-          {/* <Form.Item
-            name="birthday"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <DatePicker placeholder="생년월일 8자리" disabledDate={disabledDate} />
-          </Form.Item> */}
           <Form.Item
             name="birthday"
             rules={[
@@ -179,6 +190,7 @@ const Join = () => {
                 message: "올바른 전화번호 양식이 아닙니다.",
               },
             ]}
+            validateTrigger="onBlur"
           >
             <Input placeholder="휴대전회번호('-' 제외하고 입력)" onChange={handlePhoneNumberChange} maxLength="13" />
           </Form.Item>
@@ -197,15 +209,11 @@ const Join = () => {
             </Radio.Group>
           </Form.Item>
 
-          <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
-            <Upload.Dragger name="files" action="/upload.do">
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              {/* <p className="ant-upload-text">Click or drag file to this area to upload</p> */}
-              <p className="ant-upload-hint">최대 10mb이하 png,jpg,jpge,gif</p>
-            </Upload.Dragger>
-          </Form.Item>
+          {showAuthenticationCodeField && (
+            <Form.Item name="authenticationCode" rules={[{ required: true, message: "인증번호를 입력하세요." }]}>
+              <Input placeholder="인증번호 입력" />
+            </Form.Item>
+          )}
         </div>
         <Form.Item>
           <SubmitButton type="primary" htmlType="submit" block className="mt-8" form={form}>
@@ -213,7 +221,6 @@ const Join = () => {
           </SubmitButton>
         </Form.Item>
       </Form>
-
     </div>
   );
 };
