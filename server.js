53d7d5994d1e501b6988d77a01833aa9365bf010
taga-dysong <116068642+butcreep@users.noreply.github.com>
2024-05-16 13:24:05 +0900

    server.js 재배포

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-// Vercel의 환경 변수를 사용하여 포트를 설정
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
